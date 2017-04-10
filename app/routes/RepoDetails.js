9a2c0347272a44c2ed1c6e079bb48e87b03f863e
JetQin <qinjet@gmail.com>
2017-04-10 22:18:13 +0800

    fix bug

--- app/routes/RepoDetails.js
+++ app/routes/RepoDetails.js
@@ -28,7 +28,7 @@ class RepoDetails extends Component {
     render() {
         let stars = [];
         for (var index = 0; index < this.state.repository.stargazers_count; index++) {
-            stars.push('🌟');
+            stars.push('🌟git');
         }
         return (
             <div>
